0a6bd446037303d9a39520f0b4caa4abc9eea4de
Philip Browne <pbrowne@gmail.com>
2022-01-20 12:26:52 -0500

    add readme

---
+++ README.md
@@ -0,0 +1,37 @@
+# GoLang Instruments API
+
+Example of a basic REST API with Musical Instruments that runs CRUD functions.
+
+## Local Deployment in Project Folder
+
+`go run main.go app.go`
+
+Local Base URL is localhost:8000
+
+## Routes
+
+**GET /api/v1/instruments** - Returns a list of all instruments
+
+**GET /api/v1/instruments/{id}** - Returns an instrument by ID
+
+**POST /api/v1/instruments** - Create a new instrument
+
+Example Request Body:
+
+    {
+        "name" : "Tuba,
+        "family" : "Brass",
+        "price" : 3496.43
+    }
+
+**PUT /api/v1/instruments/{id}** - Updates an existing instrument by Id
+
+Example Request Body:
+
+    {
+        "name" : "Tuba,
+        "family" : "Brass",
+        "price" : 9943.43
+    }
+
+**DELETE /api/v1/instruments/{id}** - Deletes an existing instrument
--- main.go
+++ main.go
@@ -1,37 +1,7 @@
 package main
 
-// // Get All Instruments
-// func getInstruments(w http.ResponseWriter, r *http.Request){
-
-// }
-// // Get One Instrument
-// func getInstruments(w http.ResponseWriter, r *http.Request){
-
-// }
-
-// // Get All Instruments
-// func getInstruments(w http.ResponseWriter, r *http.Request){
-
-// }
-// // Get All Instruments
-// func getInstruments(w http.ResponseWriter, r *http.Request){
-
-// }
-
-// func insert(collection *mongo.Collection, inst {}){
-// 	instrument := Instrument{
-// 		count(collection), inst.Name, inst.Family, inst.Price
-// 	}
-// 	insertResult, err := collection.InsertOne(context.TODO(), instrument)
-// 	if err != nil {
-//     	log.Fatal(err)
-// 	}
-// 	fmt.Println(insertResult)
-// }
-
-
 func main(){
 	a := App{}
 	a.Initialize()
-	a.Run(":8080")
+	a.Run(":8000")
 }
